5a6d439de49807cd50fbd2f16e32380762d5abfe
Ali n <faure7n@gmail.com>
2024-07-24 14:23:59 +0330

    fix: responsive style

--- src/components/Home/Header.tsx
+++ src/components/Home/Header.tsx
@@ -1,37 +1,44 @@
-import React from 'react'
-import './Header.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from 'react';
+import './Header.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router-dom';
 
 const Header: React.FC = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const navigate = useNavigate();
 
   const handleHomeClick = () => {
     navigate('/');
-  }
+    setIsMenuOpen(false);
+  };
   const handleCartClick = () => {
     navigate('/cart');
-  }
+    setIsMenuOpen(false);
+  };
   const handleViewModelsClick = () => {
-    navigate('/models')
-  }
+    navigate('/models');
+    setIsMenuOpen(false);
+  };
   const handleLoginClick = () => {
-    navigate('/login')
-  }
+    navigate('/login');
+    setIsMenuOpen(false);
+  };
   const handleUserSettingClick = () => {
-    navigate('/user-setting')
-  }
+    navigate('/user-setting');
+    setIsMenuOpen(false);
+  };
 
-
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
     <div className='header'>
-      <img className='header-logo'
+      <img onClick={handleHomeClick} className='header-logo'
         src='https://i.pinimg.com/originals/20/60/2d/20602d43cc993811e5a6bd1886af4f33.png' alt='Logo' />
-
       <div className='header-center'>
-        <ul>
+        <ul className={`nav-links ${isMenuOpen ? 'active' : ''}`}>
           <li onClick={handleHomeClick}>Home</li>
           <li>About</li>
           <li>Contact us</li>
@@ -50,8 +57,14 @@ const Header: React.FC = () => {
           </li>
         </ul>
       </div>
+
+      <div className='burger' onClick={toggleMenu}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Header;
